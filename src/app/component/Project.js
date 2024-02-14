afe9ed26c438fdc3aacc19889e40aa1c306b746f
Muhammad-Sh0aib <shoaibbabar098@gmail.com>
2024-02-14 14:43:21 +0500

    add weather-app project

--- src/app/component/Project.js
+++ src/app/component/Project.js
@@ -1,6 +1,7 @@
 import styles from "../Style/project.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faTable } from "@fortawesome/free-solid-svg-icons";
+import { faCloudBolt } from "@fortawesome/free-solid-svg-icons";
 
 const Project = () => {
   return (
@@ -10,26 +11,23 @@ const Project = () => {
         <h6 className={"subHeading"}>my workings</h6>
       </div>
 
+
       <div className={styles.CardsContainer}>
 
-
-      
+        {/* Budget app card */}
         <div className={styles.proCard}>
-
           <a
-              href="https://muhammad-sh0aib.github.io/Budget_web/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-          <h2>Budget App</h2>
-          <div className={styles.iconCricle}>
-            <FontAwesomeIcon className={styles.codeIcon} icon={faCode} />
-          </div>
-          <p>Budget calculator app using Html Css and JavaScript</p>
-
-
+            href="https://muhammad-sh0aib.github.io/Budget_web/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Budget App</h2>
+            <div className={styles.iconCricle}>
+              {/* <FontAwesomeIcon className={styles.codeIcon} icon={faCode} /> */}
+              <FontAwesomeIcon className={styles.codeIcon} icon={faTable} />
+            </div>
+            <p>Budget calculator app using Html Css and JavaScript</p>
           </a>
-
           <div className={styles.cardBtn}>
             <a
               href="https://github.com/Muhammad-Sh0aib/Budget_web.git"
@@ -40,7 +38,6 @@ const Project = () => {
                 <h6>Source Code</h6>
               </div>
             </a>
-
             <a
               href="https://muhammad-sh0aib.github.io/Budget_web/"
               target="_blank"
@@ -50,16 +47,47 @@ const Project = () => {
                 <h6>Live Project</h6>
               </div>
             </a>
-
-
           </div>
-          
         </div>
 
-        
-
+        {/* Weather app card */}
+        <div className={styles.proCard}>
+          <a
+            href="https://weather-app-sigma-lilac.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Weather App</h2>
+            <div className={styles.iconCricle}>
+              <FontAwesomeIcon className={styles.codeIcon} icon={faCloudBolt} />
+            </div>
+            <p>React.js weather app fetching live weather details from an API.</p>
+          </a>
+          <div className={styles.cardBtn}>
+            <a
+              href="https://github.com/Muhammad-Sh0aib/Weather_app.git"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.btn}>
+                <h6>Source Code</h6>
+              </div>
+            </a>
+            <a
+              href="https://weather-app-sigma-lilac.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.btn}>
+                <h6>Live Project</h6>
+              </div>
+            </a>
+          </div>
+        </div>
 
       </div>
+
+
     </div>
   );
 };
