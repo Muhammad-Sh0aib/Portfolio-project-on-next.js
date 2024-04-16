2600058b533076b38ae878cf6f8b283463700768
Muhammad-Sh0aib <shoaibbabar098@gmail.com>
2024-04-16 21:50:13 +0500

    update home section

--- src/app/component/Home.js
+++ src/app/component/Home.js
@@ -3,11 +3,12 @@ import styles from "../Style/home.module.scss";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
-  useEffect(()=>{
-    AOS.init({duration: "1000"})
-  },[])
+  useEffect(() => {
+    AOS.init({ duration: "1000" })
+  }, [])
   return (
     <div className={styles.HomeSection} id="home">
 
@@ -28,7 +29,27 @@ const Home = () => {
         <div className={styles.content}>
           <h6>Hi, I'm</h6>
           <h2>Muhammad Shoaib</h2>
-          <h6>Frontend Developer</h6>
+          {/* <h6>Frontend Developer</h6> */}
+
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              'Fronten Developer',
+              1000, // wait 1s before replacing "Mice" with "Hamsters"
+              'Web Developer',
+              1000,
+              'Web Designer',
+              1000,
+              'Ui/Ux Designer',
+              1000
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: '2em', display: 'inline-block' }}
+            repeat={Infinity}
+          />
+
+
 
           <div className="mt-4">
 
@@ -54,7 +75,7 @@ const Home = () => {
             </div>
 
             <a href="./CV.pdf" download>
-            <button className={"btn"}>Download CV</button>
+              <button className={"btn"}>Download CV</button>
             </a>
           </div>
         </div>
